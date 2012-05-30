62e980b51e867e446eccd77c43389884a8a04bf3
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-05-30 16:05:09 -0700

    Add eclipse-restore.sh
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

---
+++ eclipse-restore.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+
+#
+# Rebuild the eclipse stuff after in a hadoop/ directory after it's
+# been clobbered
+#
+
+die() {
+    echo $@
+    exit 1
+}
+
+P="`readlink -f .`"
+if [ "x${P}" == "x/home/cmccabe/hadoop1" ]; then 
+    W=/home/cmccabe/workspace
+elif [ "x${P}" == "x/home/cmccabe/hadoop2" ]; then 
+    W=/home/cmccabe/workspace2
+else
+    die "unknown directory '${P}'"
+fi
+
+mvn clean
+mvn package -e -Pdist -Dtar -DskipTests -Dmaven.javadoc.skip=true
+mvn -Declipse.workspace=${W} eclipse:configure-workspace
+mvn -DdownloadSources=true -DdownloadJavadocs=true eclipse:eclipse
+cp -a hadoop-hdfs-project/hadoop-hdfs/target/webapps \
+    hadoop-hdfs-project/hadoop-hdfs/target/classes/webapps

195e9a3f298375d40d1714d7e16dd40e58b91c82
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-06-26 12:30:11 -0700

    jarjar.sh: add LD_LIBRARY_PATH
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- jarjar.sh
+++ jarjar.sh
@@ -10,4 +10,10 @@ if [ $# -eq 0 ]; then
 else
     TGT_DIR=$1
 fi
-export CLASSPATH=$( find ${TGT_DIR} -noleaf -type f -name '*.jar' -printf ':%p' | cut -b 2- )
+export CLASSPATH=$( 
+    find ${TGT_DIR} -noleaf -type f -name '*.jar' -printf ':%p' | cut -b 2- \
+)
+export LD_LIBRARY_PATH=$( 
+    find ${TGT_DIR} -noleaf -name '*.so' -print0 | \
+        xargs -0 -l dirname | xargs -l printf ':%s' | cut -b 2-
+)

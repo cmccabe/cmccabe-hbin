2ea5f102367543ea83d846739cd203ec286e9f6c
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-08-21 16:28:44 -0700

    jarjar.sh: allow multiple directories to be given
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- jarjar.sh
+++ jarjar.sh
@@ -6,15 +6,15 @@
 #
 
 if [ $# -eq 0 ]; then
-    TGT_DIR=.
+    TGT_DIRS=.
 else
-    TGT_DIR=$1
+    TGT_DIRS=$@
 fi
 export CLASSPATH=$( 
-    find ${TGT_DIR} -noleaf -type f -name '*.jar' -printf ':%p' | cut -b 2- \
+    find ${TGT_DIRS} -noleaf -type f -name '*.jar' -printf ':%p' | cut -b 2- \
 )
-export LD_LIBRARY_PATH=$( 
-    find ${TGT_DIR} -noleaf -name '*.so' -print0 | \
-        xargs -0 -l dirname | egrep '(target/native/target|.libs)' | \
-        xargs -l printf ':%s' | cut -b 2-
-)
+#export LD_LIBRARY_PATH=$( 
+    #find ${TGT_DIR} -noleaf -name '*.so' -print0 | \
+        #xargs -0 -l dirname | egrep '(target/native/target|.libs)' | \
+        #xargs -l printf ':%s' | cut -b 2-
+#)

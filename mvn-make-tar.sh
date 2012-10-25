7fcd1659c16009de98ae02c70eaffed5206224fe
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-10-25 11:22:03 -0700

    mvn-make-tar.sh: run with -X in addition to -e
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- mvn-make-tar.sh
+++ mvn-make-tar.sh
@@ -2,4 +2,4 @@
 
 rm -f ./hadoop-dist/target/hadoop-*-SNAPSHOT.tar.gz
 
-time mvn package -e -Drequire.fuse -Drequire.snappy -Pnative -Pdist -Dtar -DskipTests -Dmaven.javadoc.skip=true
+time mvn package -Drequire.fuse -Drequire.snappy -Pnative -Pdist -Dtar -DskipTests -Dmaven.javadoc.skip=true -X -e

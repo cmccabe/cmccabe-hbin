f3ddce03f89a0265366682dba364bf3956cd5ccd
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-10-30 14:02:18 -0700

    rwblast/run: Add 'time' to allowable perf ops
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- rwblast/run.sh
+++ rwblast/run.sh
@@ -17,14 +17,17 @@ Also comment "Defaults env_reset" if it's present.
 
 -h:               this help message
 -n <num-copies>:  number of copies of the file to read
+-p <perf-op>:     perf operation(s) (default: stat, suggested: time, record)
 EOF
 }
 
 num_copies=1
-while getopts  "f:hn:" flag; do
+perf_op="stat"
+while getopts  "f:hn:p:" flag; do
     case $flag in
     h) usage; exit 0;;
     n) num_copies=$OPTARG;;
+    p) perf_op=$OPTARG;;
     *) echo; usage; exit 1;;
     esac
 done
@@ -37,5 +40,9 @@ echo "/h/bin/hadoop fs -cat $rwblasts > /dev/null" > $TMPSCRIPT \
 chmod +x $TMPSCRIPT || die "failed to chmod $TMPSCRIPT"
 orig_user=`whoami`
 echo "** running $(cat $TMPSCRIPT) **"
-sudo -E perf stat -a sudo -E -u $orig_user $TMPSCRIPT \
-    || die "copyFromLocal failed."
+if [ "x$perf_op" == "xtime" ]; then
+    /usr/bin/time $TMPSCRIPT || die "$TMPSCRIPT failed"
+else
+    sudo -E perf ${perf_op} -a sudo -E -u $orig_user $TMPSCRIPT \
+        || die "$TMPSCRIPT failed."
+fi

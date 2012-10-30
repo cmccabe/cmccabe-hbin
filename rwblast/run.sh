9726221b5d8506b1fd196639c3185beb3a54f8fa
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-10-30 14:16:16 -0700

    rwblsat/run.sh: add reps (-r) argument
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- rwblast/run.sh
+++ rwblast/run.sh
@@ -18,26 +18,31 @@ Also comment "Defaults env_reset" if it's present.
 -h:               this help message
 -n <num-copies>:  number of copies of the file to read
 -p <perf-op>:     perf operation(s) (default: stat, suggested: time, record)
+-r <reps>:        number of times to repeat the operation under test
 EOF
 }
 
 num_copies=1
 perf_op="stat"
-while getopts  "f:hn:p:" flag; do
+reps=1
+while getopts  "f:hn:p:r:" flag; do
     case $flag in
     h) usage; exit 0;;
     n) num_copies=$OPTARG;;
     p) perf_op=$OPTARG;;
+    r) reps=$OPTARG;;
     *) echo; usage; exit 1;;
     esac
 done
 
 TMPSCRIPT=`mktemp -t rwblast.XXXXXXXXXX` || exit 1
 trap "rm -rf ${TMPSCRIPT}; exit" INT TERM EXIT
-rwblasts=`for i in $(seq -s ' ' -w 1 $num_copies); do echo -n "/rwblast-$i "; done`
-echo "/h/bin/hadoop fs -cat $rwblasts > /dev/null" > $TMPSCRIPT \
-    || die "failed to create $TMPSCRIPT"
-chmod +x $TMPSCRIPT || die "failed to chmod $TMPSCRIPT"
+filelist=`for i in $(seq -s ' ' -w 1 $num_copies); do echo -n "/rwblast-$i "; done`
+rwblasts=`for i in $(seq -s ' ' -w 1 $reps); do echo -n "$filelist "; done`
+cat <<EOF > "$TMPSCRIPT" || die "failed to create $TMPSCRIPT"
+/h/bin/hadoop fs -cat $rwblasts > /dev/null
+EOF
+chmod +x "$TMPSCRIPT" || die "failed to chmod $TMPSCRIPT"
 orig_user=`whoami`
 echo "** running $(cat $TMPSCRIPT) **"
 if [ "x$perf_op" == "xtime" ]; then

1e49bcd6302e06ec557a8fccc3db2c4e34234f45
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-11-09 13:16:28 -0800

    nn-ha-do.sh: add -redirect option
    This option allows the caller to redirect stdout / stderr to a file
    before running the command as the desired entity.
    
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- nn-ha-do.sh
+++ nn-ha-do.sh
@@ -8,6 +8,14 @@ die() {
 [ $# -lt 1 ] && die "you must supply an HA node to do the command as."
 HA=$1
 shift
+[ $# -lt 1 ] && die "you must supply the command to be done."
+if [ $1 == "-redirect" ]; then
+    shift
+    [ $# -lt 1 ] && die "you must supply the file to redirect output to."
+    REDIRECT_FILE=$1
+    shift
+    exec &>$REDIRECT_FILE # redirect stdout / stderr to the file.
+fi
 PREV_HADOOP_CONF_DIR="$HADOOP_CONF_DIR"
 [ -d "$PREV_HADOOP_CONF_DIR" ] || die "can't find \$HADOOP_CONF_DIR=$HADOOP_CONF_DIR"
 source $PREV_HADOOP_CONF_DIR/$HA/hadoop-env.sh

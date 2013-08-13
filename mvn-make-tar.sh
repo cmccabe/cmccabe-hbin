6071a0ed051a59b4cb77f94a7562585b53154cc8
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2013-08-13 13:54:51 -0700

    fix for protobuf mess
    We now select the correct protoc (by default under /opt) based on the
    hdfs pom file...
    
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- mvn-make-tar.sh
+++ mvn-make-tar.sh
@@ -2,4 +2,14 @@
 
 rm -f ./hadoop-dist/target/hadoop-*-SNAPSHOT.tar.gz
 
-time mvn package -Drequire.fuse -Drequire.snappy -Drequire.libwebhdfs -Pnative -Pdist -Dtar -DskipTests -Dmaven.javadoc.skip=true "$@"
+# Add the correct version of protoc to the front of the PATH.
+if grep -q '<protocVersion>2.5.0</protocVersion>' ./hadoop-hdfs-project/hadoop-hdfs/pom.xml; then
+    PROTOBUF_2_5="${PROTOBUF_2_5:-/opt/protobuf-2.5/bin}"
+    PATH="$PROTOBUF_2_5:$PATH"
+else
+    PROTOBUF_2_4="${PROTOBUF_2_4:-/opt/protobuf-2.4/bin}"
+    PATH="$PROTOBUF_2_4:$PATH"
+fi
+
+time mvn package -Drequire.fuse -Drequire.snappy -Drequire.libwebhdfs \
+    -Pnative -Pdist -Dtar -DskipTests -Dmaven.javadoc.skip=true "$@"

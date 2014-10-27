a1ba42dc3eb1f492027b4f121419551175eb2439
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2014-10-27 14:56:26 -0700

    mvn-make-tar.sh: add -Drequire.openssl
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- mvn-make-tar.sh
+++ mvn-make-tar.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(dirname "$0")"
 rm -f ./hadoop-dist/target/hadoop-*-SNAPSHOT.tar.gz
 
 time mvn package -Drequire.fuse \
-    -Pnative -Pdist -Dtar -DskipTests -Dmaven.javadoc.skip=true "$@"
+    -Pnative -Pdist -Dtar -DskipTests -Drequire.openssl -Dmaven.javadoc.skip=true "$@"

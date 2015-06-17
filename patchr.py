29c8bf6e5008c25c371fea19f2bd5c23c66c421d
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2015-06-17 13:25:41 -0700

    patchr.py: add -p option, ability to run on older Pythons
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- patchr.py
+++ patchr.py
@@ -11,11 +11,19 @@ import re
 import subprocess
 import sys
 
+from subprocess import PIPE,Popen
+
+def run_check_output(what):
+    proc = Popen(what, stdout=PIPE)
+    return (proc.communicate()[0].strip())
+
 listdir = os.path.join(os.environ["HOME"], "list")
 jira = sys.argv[1]
 branch = sys.argv[2]
 branch_suffix = None
 if (branch == "trunk"):
+    branch_suffix = ""
+elif (branch == "master"):
     branch_suffix = ""
 elif (branch == "branch-1"):
     branch_suffix = "-b1"
@@ -34,16 +42,25 @@ elif (branch == "fs-encryption"):
 else:
     raise RuntimeError("can't understand branch %s" % branch)
 overwrite  = False
+mkdirs = False
 if (len(sys.argv) >= 4):
-    if (sys.argv[3] == "-f"):
-        overwrite = True
-    else:
-        raise RuntimeError("can't understand option %s" % sys.argv[3])
+    remaining = sys.argv[3:]
+    while len(remaining) > 0:
+        if remaining[0] == "-f":
+            overwrite = True
+        elif remaining[0] == "-p":
+            mkdirs = True
+        else:
+            raise RuntimeError("can't understand option %s" % remaining[0])
+        remaining = remaining[1:]
 gpat = listdir + "/" + jira + "_*/"
 print "gpat = " + gpat
 paths = glob.glob(gpat)
 if (len(paths) == 0):
-    raise RuntimeError("No directory for " + jira)
+    if not mkdirs:
+        raise RuntimeError("No directory for " + jira)
+    paths.append(listdir + "/" + jira + "_jira");
+    os.makedirs(paths[0]);
 for p in paths[1:]:
     if os.path.isdir(p):
         raise RuntimeError("more than one directory for " + jira)
@@ -66,11 +83,11 @@ if (overwrite):
         next_patch_num = highest_patch_num
 else:
     next_patch_num = highest_patch_num + 1
-commit = subprocess.check_output([ "git", "merge-base", "HEAD", branch]).rstrip()
+commit = run_check_output([ "git", "merge-base", "HEAD", branch])
 cmd = [ "git", "diff", "--binary", commit, "HEAD" ]
 outfile = "%s/%s%s.%03d.patch" % (dir_path, jira, branch_suffix, next_patch_num)
 print " ".join(cmd) + " > " + outfile
-patch = subprocess.check_output(cmd)
+patch = run_check_output(cmd)
 f = open(outfile, "w")
 try:
     f.write(patch)

75df3cc2851e187721fa80752caf3d037d69cd87
Colin P. McCabe <cmccabe@apache.org>
2019-08-01 11:58:06 -0700

    go-env-wrapper.sh: improve console output

--- go-env-wrapper.sh
+++ go-env-wrapper.sh
@@ -1,7 +1,6 @@
 eval "$(goenv init -)"
-set -x
-echo $PATH
 VERSION=$(goenv version | awk '{print $1}')
 export PATH="${PATH}:/home/cmccabe/go/$VERSION/bin"
-export PARH="$(/home/cmccabe/cmccabe-bin/path-fixer)"
-set +x
+export PATH="$(/home/cmccabe/cmccabe-bin/path-fixer)"
+echo -n "** setting PATH="
+echo $PATH | sed 's/:/\n/g'

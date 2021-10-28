aefc3c5234f13918b27edab6e1ff6530b496ef39
Colin P. McCabe <cmccabe@apache.org>
2021-10-28 09:26:50 -0700

    Add find-hanging-tests.go

---
+++ find-hanging-tests.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+
+func main() {
+	activeMap := make(map[string]bool)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		s := scanner.Text()
+		rightBracketIndex := strings.Index(s, "]")
+		if rightBracketIndex > 0 {
+			handleLine(s[rightBracketIndex+2:], activeMap)
+		}
+	}
+	printMap(activeMap)
+}
+
+func handleLine(s string, activeMap map[string]bool) {
+	startedIndex := strings.Index(s, "STARTED")
+	if startedIndex > 0 {
+		activeMap[s[:startedIndex]] = true
+		return
+	}
+	passedIndex := strings.Index(s, "PASSED")
+	if passedIndex > 0 {
+		delete(activeMap, s[:passedIndex])
+		return
+	}
+	failedIndex := strings.Index(s, "FAILED")
+	if failedIndex > 0 {
+		delete(activeMap, s[:failedIndex])
+		return
+	}
+}
+
+func printMap(activeMap map[string]bool) {
+	for t := range(activeMap) {
+		fmt.Printf("%s\n", t)
+	}
+}

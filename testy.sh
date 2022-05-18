a15ac161706544a2e465da57e4bfb9be8a2fd0e3
Colin P. McCabe <cmccabe@apache.org>
2022-05-18 11:32:37 -0700

    Add testy.sh (kafka test tool)

---
+++ testy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+die() {
+    echo $@
+    exit 1
+}
+
+[[ $# -lt 1 ]] && die "Usage: $0 [component] [test]"
+COMPONENT=$1
+shift
+
+FLAGS_TO_APPEND=""
+while [[ $# -gt 0 ]]; do
+    FLAGS_TO_APPEND="$FLAGS_TO_APPEND --tests ${1}"
+    shift
+done
+#--offline 
+exec ./gradlew \
+    :${COMPONENT}:test \
+    -x :${COMPONENT}:checkstyleMain \
+    -x :${COMPONENT}:checkstyleTest \
+    -x :${COMPONENT}:spotbugsMain \
+    -x :${COMPONENT}:spotbugsTest \
+    ${FLAGS_TO_APPEND}
